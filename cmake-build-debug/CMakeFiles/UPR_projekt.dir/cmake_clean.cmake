f0f45ed49fd1718e7c62b986adfc01f139d260f0
Ondřej Just <ondrajust@seznam.cz>
2021-11-18 17:03:41 +0100

    Created project

---
+++ cmake-build-debug/CMakeFiles/UPR_projekt.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/UPR_projekt.dir/main.c.obj"
+  "UPR_projekt.exe"
+  "UPR_projekt.exe.manifest"
+  "UPR_projekt.lib"
+  "UPR_projekt.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/UPR_projekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/UPR_projekt.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for UPR_projekt.
+# This may be replaced when dependencies are built.
